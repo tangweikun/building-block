3ccc4b3d9c59fbe1cb30d1c85fa3538fce64dffb
tangweikun <819573105@qq.com>
2018-11-01 10:32:59 +0800

    :fire: Remove old project

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export * from './button'
-export * from './modal'
-export * from './loading'
-export * from './noData'
--- src/components/loading/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './index.css'
-import LOADING_IMG from 'assets/loading.gif'
-
-export const Loading = () => (
-  <div className="loading-wrapper">
-    <img src={LOADING_IMG} alt="loading" />
-  </div>
-)
