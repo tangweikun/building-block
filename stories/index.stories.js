ea70bee55a86167c1eae2c4d7f4f67f3f3f21209
tangweikun <819573105@qq.com>
2018-11-21 16:22:17 +0800

    :memo: Update stories

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -7,10 +7,10 @@ import {
   ButtonDemo,
 } from '../examples'
 
+storiesOf('Button', module).add('SexyButton', () => <ButtonDemo />)
+
 storiesOf('Tooltip', module).add('basic', () => <TooltipDemo />)
 
 storiesOf('ScrollContainer', module).add('basic', () => <ScrollContainerDemo />)
 
 storiesOf('Card', module).add('basic', () => <CardDemo />)
-
-storiesOf('Button', module).add('basic', () => <ButtonDemo />)
